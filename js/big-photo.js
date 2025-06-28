6c304998931bbe8684c983a65975d7644934f288
oleg <oleg.letit@gmail.com>
2025-06-28 12:19:05 +0300

    8.8. Перламутровые пуговицы (часть 2)

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,21 +1,18 @@
 import { isEscapeKey, CLASS_HIDDEN, CLASS_MODAL_OPEN, body } from './util.js';
 
+const STEP_PER_PORTION = 5;
+
 const bigPicture = document.querySelector('.big-picture');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 const closeModalButton = bigPicture.querySelector('.big-picture__cancel');
 
-const onDocumentKeydown = () => {
-  if (isEscapeKey) {
-    onCloseButtonClick();
-  }
-};
-
-const onCloseButtonClick = () => {
-  bigPicture.classList.add(CLASS_HIDDEN);
-  body.classList.remove(CLASS_MODAL_OPEN);
-  document.removeEventListener('keydown', onDocumentKeydown);
-  closeModalButton.removeEventListener('click', onCloseButtonClick);
-};
+const commentList = bigPicture.querySelector('.social__comments');
+const loadMoreButton = bigPicture.querySelector('.comments-loader');
+const bigPhotoImg = bigPicture.querySelector('.big-picture__img > img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const shownCommentsCount = bigPicture.querySelector('.social__shown-comments-count');
+const socialCaption = bigPicture.querySelector('.social__caption');
 
 const createComment = (comment) => {
   const newComment = commentTemplate.cloneNode(true);
@@ -28,32 +25,60 @@ const createComment = (comment) => {
 };
 
 const renderBigPhoto = (photo) => {
-  const fragment = document.createDocumentFragment();
-  const commentList = bigPicture.querySelector('.social__comments');
   const comments = photo.comments;
-
-  document.addEventListener('keydown', onDocumentKeydown);
-  closeModalButton.addEventListener('click', onCloseButtonClick);
-
-  bigPicture.querySelector('.social__comment-count').classList.add(CLASS_HIDDEN);
-  bigPicture.querySelector('.comments-loader').classList.add(CLASS_HIDDEN);
+  let shownComments = 0;
 
   body.classList.add(CLASS_MODAL_OPEN);
 
-  bigPicture.querySelector('.big-picture__img > img').src = photo.url;
-  bigPicture.querySelector('.likes-count').textContent = photo.likes;
-  bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
-  bigPicture.querySelector('.social__caption').textContent = photo.description;
-
-  comments.forEach((comment) => {
-    fragment.appendChild(createComment(comment));
-  });
+  bigPhotoImg.src = photo.url;
+  likesCount.textContent = photo.likes;
+  commentsCount.textContent = comments.length;
+  socialCaption.textContent = photo.description;
 
   commentList.replaceChildren();
-  commentList.appendChild(fragment);
+  loadMoreButton.classList.remove(CLASS_HIDDEN);
 
+  const renderCommentsPortion = () => {
+    const portion = comments.slice(shownComments, shownComments + STEP_PER_PORTION);
+    const fragment = document.createDocumentFragment();
+
+    portion.forEach((comment) => {
+      fragment.appendChild(createComment(comment));
+    });
+
+    commentList.appendChild(fragment);
+    shownComments += portion.length;
+    shownCommentsCount.textContent = shownComments;
+
+    if (shownComments >= comments.length) {
+      loadMoreButton.classList.add(CLASS_HIDDEN);
+    }
+  };
+
+  const onLoadMoreButtonClick = () => {
+    renderCommentsPortion();
+  };
+
+  const closeBigPhoto = () => {
+    bigPicture.classList.add(CLASS_HIDDEN);
+    body.classList.remove(CLASS_MODAL_OPEN);
+    document.removeEventListener('keydown', onDocumentKeydown);
+    closeModalButton.removeEventListener('click', closeBigPhoto);
+    loadMoreButton.removeEventListener('click', onLoadMoreButtonClick);
+  };
+
+  function onDocumentKeydown(evt) {
+    if (isEscapeKey(evt.key)) {
+      closeBigPhoto();
+    }
+  }
+
+  document.addEventListener('keydown', onDocumentKeydown);
+  closeModalButton.addEventListener('click', closeBigPhoto);
+  loadMoreButton.addEventListener('click', onLoadMoreButtonClick);
+
+  renderCommentsPortion();
   bigPicture.classList.remove(CLASS_HIDDEN);
-
 };
 
 export { renderBigPhoto };
